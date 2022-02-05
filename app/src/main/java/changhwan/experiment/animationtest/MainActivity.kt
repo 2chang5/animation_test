d9cf0db32f5b184e9395451fc8bfdbdaa2360b73
changhwan <chang5hustle@gmail.com>
2022-02-05 17:00:05 +0900

    [feat] xml(set)로 textcolor 애니메이션 줘봤음

--- app/src/main/java/changhwan/experiment/animationtest/MainActivity.kt
+++ app/src/main/java/changhwan/experiment/animationtest/MainActivity.kt
@@ -1,13 +1,28 @@
 package changhwan.experiment.animationtest
 
+import android.animation.AnimatorInflater
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import changhwan.experiment.animationtest.databinding.ActivityMainBinding
 import com.spark.android.ui.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Handler(Looper.getMainLooper()).postDelayed({
+            initAnimation()
+        },2000)
+
+    }
+
+    fun initAnimation(){
+        val animationColorChange = AnimatorInflater.loadAnimator(this, R.animator.object_ani_test).apply{
+            setTarget(binding.tvAnimationText)
+            start()
+        }
+
+
     }
 }
