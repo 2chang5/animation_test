9aa9be18172bd3e0a1adff57208e6ee216c6a1d9
changhwan <chang5hustle@gmail.com>
2022-02-05 16:42:09 +0900

    [layout] 움직일 텍스트 뷰 +버튼생성

--- app/src/main/java/changhwan/experiment/animationtest/MainActivity.kt
+++ app/src/main/java/changhwan/experiment/animationtest/MainActivity.kt
@@ -2,8 +2,10 @@ package changhwan.experiment.animationtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import changhwan.experiment.animationtest.databinding.ActivityMainBinding
+import com.spark.android.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
